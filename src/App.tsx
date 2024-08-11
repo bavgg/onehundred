ee83cf7de4a12c1fd469ca4225586452a5c1f8ac
bavgg <gestopajonas@gmail.com>
2024-08-12 07:56:38 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-50">
+        <span>Hello, World!</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
